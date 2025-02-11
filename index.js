2ad5fb67ea490e545da11bc8161d60bbb44cfe65
gashman878 <gashman878@gmail.com>
2025-02-11 19:56:39 +0100

    little-changes

---
+++ index.js
@@ -0,0 +1,12 @@
+function Hello(){
+    document.getElementById("try").innerHTML = "Sorry, Hello Every One!";
+}
+function Datetime(){
+    document.getElementById("time").innerHTML = Date();
+}
+function fontS(){
+    document.getElementById('try').style.fontSize='40px';
+}
+function delet(){
+    document.getElementById('nonesense').style.display='none';
+}
