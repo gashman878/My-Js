047b4474f4c019436cfce4970bf30b48671cceff
gashman878 <gashman878@gmail.com>
2025-02-13 15:14:43 +0100

    Icommittojs

---
+++ index2.js
@@ -0,0 +1,13 @@
+function ctime() {
+    document.getElementById('ctime').innerHTML=Date();
+}
+function ltry() {
+    document.getElementById('ltry').innerHTML = "Welcome to my page";
+} //Used this function to try the onload attribute buh it didn't work shaa
+
+let word = "Hello my G";
+//This is a string.
+
+let maany = word.length
+//The value of this string is the number of alphabets in the word string earlier declared
+//You must assign the lenght as a string. You can call it straight with the .lenght keyword
